bc21137b8e9a17832203e53b45009a72e00f43bf
TomDev <andradestom@gmail.com>
2022-06-17 23:04:01 -0300

    service and server ready

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,19 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { TASKS } from '../../mock-tasks';
+import { TaskService } from '../../service/task.service';
 import { Task } from '../../task';
-
-
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
 
-  constructor() { }
+  constructor(
+    private taskService: TaskService
+  ) { }
 
   ngOnInit(): void {
+    this.taskService.getTasks().subscribe(tasks =>
+      this.tasks = tasks);
   }
 
 }
